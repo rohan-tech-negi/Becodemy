a4c6cc6de596fd682138b78c8b67c751669e581a
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-18 14:53:23 +0530

    fixd some issues and arrang the project in a single src folder

--- shared/widgets/header/logo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const logo = () => {
-  return (
-    <div>logo</div>
-  )
-}
-
-export default logo
--- src/shared/widgets/header/header.tsx
+++ src/shared/widgets/header/header.tsx
@@ -1,16 +1,17 @@
 import Link from 'next/link'
 import React from 'react'
-import logo from './logo'
-const header = () => {
+import Logo from './Logo'
+
+const Header = () => {
   return (
     <header className='w-full sticky top-0 left-0 z-[999] border-b border-b[#000] px-10 flex items-center justify-between bg-white h-[80px] text-black'>
       <div>
         <Link href={"/"}>
-        <logo></logo>
+          <Logo />
         </Link>
       </div>
     </header>
   )
 }
 
-export default header
+export default Header
