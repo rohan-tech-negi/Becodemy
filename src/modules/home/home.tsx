48366f6307604dc80572b3c22bf877440a1fee44
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-19 17:22:17 +0530

    started working on the banner section

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,10 +1,12 @@
 import Header from '@/shared/widgets/header/header'
 import React from 'react'
+import Banner from "./features/Banner"
 
 const home = () => {
   return (
     <div>
         <Header></Header>
+        <Banner></Banner>
     </div>
   )
 }
