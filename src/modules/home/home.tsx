73f7f3e49d9f41836cd16ad0160e2028168fb54c
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-21 16:20:35 +0530

    created a benefits page fr the more enhanced view

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -2,6 +2,7 @@ import Header from '@/shared/widgets/header/header'
 import React from 'react'
 import Banner from "./features/Banner"
 import Branding from "./features/Branding" 
+import Benefits from './features/Benefits'
 
 const home = () => {
   return (
@@ -9,6 +10,7 @@ const home = () => {
         <Header></Header>
         <Banner></Banner>
         <Branding></Branding>
+        <Benefits></Benefits>
     </div>
   )
 }
