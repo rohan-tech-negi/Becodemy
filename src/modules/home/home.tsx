6d9d4741160b06544b532068796157d3080ed77e
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-21 16:05:24 +0530

    starting with todays woork

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,12 +1,14 @@
 import Header from '@/shared/widgets/header/header'
 import React from 'react'
 import Banner from "./features/Banner"
+import Brasn
 
 const home = () => {
   return (
     <div>
         <Header></Header>
         <Banner></Banner>
+        <Branding></Branding>
     </div>
   )
 }
