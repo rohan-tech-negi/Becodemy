f40d61c48253785b12b36bf79e4482ddb7208e37
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-23 01:53:06 +0530

    created a new file named sidebarfooterlogo for the appliment of some new items in the dashboarditems

--- src/shared/widgets/dashboard/sidebar/DashboardItems.tsx
+++ src/shared/widgets/dashboard/sidebar/DashboardItems.tsx
@@ -1,9 +1,11 @@
 import { sideBarBottomItems, sideBarItems } from '@/app/config/constants'
 import useRouteChange from '@/shared/hooks/useRouteChange'
+import { ICONS } from '@/shared/utils/Icons'
 import { useClerk } from '@clerk/nextjs'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+import { SidebarFooterLogo } from './SidebarFooterLogo'
 // import usePathname
 
 const DashboardItems = ({bottomContent}:{bottomContent?:boolean}) => {
@@ -57,6 +59,23 @@ const DashboardItems = ({bottomContent}:{bottomContent?:boolean}) => {
               </Link>
             )
           )}
+
+          {/* sign out */}
+          <div className="p-2 py-5 flex items-center cursor-pointer border-b"
+          
+          >
+            <span className="text-3xl mr-2">{ICONS.logOut}</span>
+            <span className="text-xl">Sign Out</span>
+          </div>
+          {/* footer */}
+          <br />
+          <br />
+          <div className="w-full flex justify-center cursor-pointer">
+            <SidebarFooterLogo/>
+          </div>
+          <p className="text-sm text-center pt-5 pb-10">
+            © 2024 Becodemy, Inc. All rights reserved.
+          </p>
           </>
         )
       }
