601cd9f27c8c1c6f052b32117c62ecb31f22ebd5
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-23 01:40:15 +0530

    started on our work on the dashboarditems

--- src/shared/widgets/dashboard/sidebar/DashboardItems.tsx
+++ src/shared/widgets/dashboard/sidebar/DashboardItems.tsx
@@ -1,6 +1,11 @@
+import useRouteChange from '@/shared/hooks/useRouteChange'
+import { useClerk } from '@clerk/nextjs'
 import React from 'react'
 
-const DashboardItems = () => {
+const DashboardItems = ({bottomContent}:{bottomContent?:boolean}) => {
+  const {activeRoute, setActiveRoute} = useRouteChange()
+  const {signOut, user} = useClerk()
+  const pathname = usePathname();
   return (
     <div>DashboardItems</div>
   )
