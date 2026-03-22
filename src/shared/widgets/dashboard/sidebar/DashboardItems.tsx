4eb0000f4f48460d6527d7807c56eed3c56d09c1
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-23 01:47:21 +0530

    displayed all the items that are available on the dashboard

--- src/shared/widgets/dashboard/sidebar/DashboardItems.tsx
+++ src/shared/widgets/dashboard/sidebar/DashboardItems.tsx
@@ -1,13 +1,37 @@
+import { sideBarItems } from '@/app/config/constants'
 import useRouteChange from '@/shared/hooks/useRouteChange'
 import { useClerk } from '@clerk/nextjs'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
+// import usePathname
 
 const DashboardItems = ({bottomContent}:{bottomContent?:boolean}) => {
   const {activeRoute, setActiveRoute} = useRouteChange()
   const {signOut, user} = useClerk()
   const pathname = usePathname();
   return (
-    <div>DashboardItems</div>
+    <>
+      {
+        !bottomContent ? (
+          <>
+            {sideBarItems.map((item:DashboardSideBarTypes, index:number)=> (
+              <Link key={index} href={item.url} className='py-2 py-5 flex items-center'>
+                  <span className={`text-3xl mr-2 ${item.url === activeRoute ? 'text-primary' : 'text-gray-500'}`}>
+                    {item.icon}
+                  </span>
+
+                  <span className={`text-xl mr-2 ${item.url === activeRoute && "text-[#463bbd]"}`}>
+                    {item.title}
+                  </span>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <></>
+        )
+      }
+    </>
   )
 }
 
