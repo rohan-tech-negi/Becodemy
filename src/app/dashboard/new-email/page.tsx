eb97e97913686aae9ee87aa54655aebee769e933
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-24 11:06:53 +0530

    working on the email making section

--- src/app/dashboard/new-email/page.tsx
+++ src/app/dashboard/new-email/page.tsx
@@ -1,9 +1,31 @@
 
 "use client"
 
+import { ICONS } from "@/shared/utils/Icons";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+
 const page = () => {
+   const searchParams = useSearchParams();
+  const subject: string = searchParams.get("subject")!;
+  const subjectTitle = subject.replace(/-/g, " ");
   return (
-    <div>page</div>
+    <div className="w-full flex bg-[#F7F7F7]">
+      <div className="w-full p-5 bg-white rounded-r-xl">
+        {/* back arrow */}
+        <Link
+          href={"/dashboard/write"}
+          className="opacity-[.7] w-min flex text-xl items-center"
+        >
+          <span>{ICONS.backArrow}</span>
+          <span>Exit</span>
+        </Link>
+        {/* email editor */}
+        <div className="my-5">
+          <Emaileditor subjectTitle={subjectTitle} />
+        </div>
+      </div>
+    </div>
   )
 }
 
