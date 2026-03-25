ebf8d5fe13801e3560d6169854652cc285b6fc52
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-26 01:18:06 +0530

    working on the view site page

---
+++ src/app/subscribe/page.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import { useClerk } from "@clerk/nextjs"
+import { useSearchParams } from "next/navigation"
+import { FormEvent, useState } from "react"
+import toast from "react-hot-toast"
+const page = () => {
+const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const searchParams = useSearchParams();
+  const username: string = searchParams.get("username")!;
+    console.log(username)
+    
+  return (
+    <div className="w-full flex flex-col items-center justify-center h-screen">
+      <div>
+        <h1 className="text-7xl pb-8 capitalize">{username} NewsLetter</h1>
+      </div>
+      <form
+        className="flex w-full max-w-md border rounded overflow-hidden"
+        onSubmit={(e) => handleSubmit(e)}
+      >
+        <input
+          type="email"
+          name="email"
+          required
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Enter your email"
+          className="px-4 py-4 w-full text-gray-700 leading-tight focus:outline-none"
+        />
+        <button
+          type="submit"
+          disabled={loading}
+          className="px-8 bg-blue-500 text-white font-bold py-4 rounded-r hover:bg-blue-600 focus:outline-none"
+        >
+          Subscribe
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default page
--- src/modules/dashboard/elements/main/main.tsx
+++ src/modules/dashboard/elements/main/main.tsx
@@ -72,8 +72,7 @@ const Main = () => {
                       copied ? "bg-blue-200" : "bg-transparent"
                     }`}
                   >
-                    {process.env.NEXT_PUBLIC_WEBSITE_URL}/subscribe?username=
-                    {user?.username}
+                    {process.env.NEXT_PUBLIC_WEBSITE_URL}/subscribe?username={user?.username}
                   </small>
                   <div className="absolute h-[38px] w-[90px] rounded-r-lg bg-[#DFE7FF] right-0 flex items-center justify-center">
                     <span className="text-lg">{ICONS.copy}</span>
