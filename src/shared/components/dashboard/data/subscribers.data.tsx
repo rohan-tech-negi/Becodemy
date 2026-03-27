613a17094df35ba336feaccab8ef656857643414
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-28 00:46:28 +0530

    done with the subscriber data showcase

--- src/shared/components/dashboard/data/subscribers.data.tsx
+++ src/shared/components/dashboard/data/subscribers.data.tsx
@@ -10,7 +10,7 @@ const SubscribersData = () => {
 
     // console.log(data);
 
-      const columns = [
+    const columns = [
     { field: "id", headerName: "ID", flex: 0.5 },
     { field: "email", headerName: "Email", flex: 0.8 },
     { field: "createdAt", headerName: "Subscribed At", flex: 0.5 },
@@ -41,7 +41,62 @@ const SubscribersData = () => {
     })
   })
   return (
-    <div>SubscribersData</div>
+     <Box m="20px">
+      <Box
+        m="40px 0 0 0"
+        height="80vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+            outline: "none",
+          },
+          "& .css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon": {
+            color: "#000",
+          },
+          "& .MuiDataGrid-sortIcon": {
+            color: "#000",
+          },
+          "& .MuiDataGrid-row": {
+            color: "#000",
+            borderBottom: "1px solid #0000001f!important",
+          },
+          "& .MuiTablePagination-root": {
+            color: "#000",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none!important",
+          },
+          "& .name-column--cell": {
+            color: "#000",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: "#A4A9FC",
+            borderBottom: "none",
+            color: "#000",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: "#fff",
+            color: "#000",
+            border: "1px solid #0000001f",
+          },
+          "& .MuiDataGrid-footerContainer": {
+            color: "#000",
+            borderTop: "none",
+            backgroundColor: "#A4A9FC",
+            borderBottomLeftRadius: "5px",
+            borderBottomRightRadius: "5px",
+          },
+          "& .MuiCheckbox-root": {
+            color: `#3462ea !important`,
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: "#000!important",
+          },
+        }}
+      >
+        <DataGrid checkboxSelection rows={rows} columns={columns} />
+      </Box>
+    </Box>
   )
 }
 
