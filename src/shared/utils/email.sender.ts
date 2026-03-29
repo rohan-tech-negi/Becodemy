c2dad902a24a613b0e900f5c80328a706a4ea658
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-29 14:18:20 +0530

    working on the aws setup

--- src/shared/utils/email.sender.ts
+++ src/shared/utils/email.sender.ts
@@ -8,3 +8,21 @@ interface Props {
   content: string;
 }
 
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: "ap-south-1"
+})
+
+AWS.config.getCredentials(function(error){
+  if(error){
+    console.log(error.stack)
+  }
+})
+
+const ses = new AWS.SES({
+  apiVersion:"2010-12-01"
+})
+
+
+}
